4e3c237d1f4fbad26d559617a5ec01f0b6ddff61
szymky <mikaroebers@proton.me>
2026-09-22 17:01:21 +0200

    readme

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,17 @@ fn main() -> Result<()> {
 
     let lexer = lexer::Token::lexer(&source);
 
+    for (token, span) in lexer.clone().spanned() {
+        match token {
+            Ok(t) => {
+                let begin = span.start;
+                let end = span.end;
+                println!("{}..{}, {:?}, {}", begin, end, t, &source[begin..end]);
+            }
+            Err(_) => panic!(),
+        }
+    }
+
     let mut parser = Parser::new(lexer);
     let ast = parser.parse_program()?;
 
